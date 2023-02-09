f6f9f265dc4da9481ab80f0ef9e8a3b36f19dfd0
Zenitsu <gui007ex@outlook.com>
2023-02-09 17:31:08 -0300

    Versão Beta 0.7
    *Linhas reduzidas e operações aumentadas!
    *Adicionado ordem de precedência dos operadores
    *Adicionados operados "se, e somente se"(<>), "se"(->), "oposto"(~)
    *Código otimizado com uma procura objetiva dos operadores, sem precisa passar por todos em cada verificação

--- Boolean-Calculator.py
+++ Boolean-Calculator.py
@@ -1,13 +1,13 @@
-
 #Versão Beta 0.5
 from os import system as sys
 
 def Clear():
     sys("cls")
+Clear()
 
-Symbols = ["~","^","v","<>","->"]
 A, B, C, D = True, False, False, False
-TestArray = [A,"v","~","(","(",B,"<>",D,"^","~",B,")","->",C,")"]
+Op_Symbols: list = ["~","^","v","<>","->"]
+TestArray: list = ["~",A,"v","(","(",B,"<>",D,"^",B,")","->","~",C,")"]
 
 def Operate(symbol: str, a: bool, b: bool):
     #Navegar pelos símbolos e escolher a operação
@@ -45,27 +45,25 @@ def Execute(array: list):
         #Remover elementos restantes dos parênteses
         Total_Deletes = len(PriorArray)+1
         array[Delete_Flag] = Execute(PriorArray)
-        for i in range(Total_Deletes):
-            array.pop(Delete_Flag+1)
+        del array[Delete_Flag+1:Delete_Flag+1+Total_Deletes]
         
     #Passar pelos valores da lista até encontrar uma operação (enquanto seu tamanho for maior que 1)
     while len(array) > 1:
-        for i in range(len(array)):
-            #Operação encontrada
-            if array[i] in Symbols:
-                print("(" + ' '.join(map(str,array)) + ")")
-                #Verificar se é um símbolo de negação
-                if array[i] == Symbols[-1]:
-                    a = array.pop(i+1)
-                    array[i] = not a
-                    break
-                #Caso não seja, verificar qual operação
-                elif array[i+1] != "~":
-                    #Armazenar e remover valor sucessor e anterior para cálculo
-                    a, b = array.pop(i-1), array.pop(i)
-                    #Retornar o resultado da operação no lugar do símbolo
-                    array[i-1] = Operate(array[i-1], a, b)
-                    break
+        #Navegar pelos símbolos em ordem de precedência
+        for symbol in Op_Symbols:
+            #Manter aquele símbolo até efetuar todos
+            while symbol in array:
+                print(" ".join(map(str, array)))
+                #Encontrar próxima operação com sinal de inversão de valor
+                operation_index = array.index(symbol)
+                if symbol == Op_Symbols[0]:
+                    a = array.pop(operation_index+1)
+                    array[operation_index] = not a
+                else:
+                    a, b = array.pop(operation_index-1), array.pop(operation_index)
+                    array[operation_index-1] = Operate(symbol, a, b)
+        
+                
     print(array[0])
     return array[0]
 
