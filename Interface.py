f844d986fc699ca8301ec927c0729d0da6a6c2b0
Eduardo de Morais Tajra <eduardotajra@edu.unifor.br>
2023-02-13 15:00:13 -0300

    Atualização da Interface

--- Interface.py
+++ Interface.py
@@ -1,7 +1,31 @@
-from Boolean_Calculator import Execute, Clear
+import Boolean_Calculator
 
-Equations_Bank: list = []
 A, B, C, D = True, False, False, False
 TestArray: list = ["~",A,"v","(","(",B,"<>",D,"^",B,")","->","~",C,")"]
+Equations_Bank: list = [TestArray]
 
-Execute(TestArray)
+#Execute(TestArray)
+
+while True:
+
+    action = input('''Calculadora Booleana
+A - Adicionar Equação
+R - Resolver Equação
+E - Sair do Código
+''').upper()
+
+    if action == "A":
+        print("\n~Adicionar Equação~\n")
+        Boolean_Calculator.Add()
+    elif action == "R":
+        print("\n~Resolver Equação~\n")
+        if len(Equations_Bank) == 0:
+            input("Função Vazia Pressione Enter para continuar...")
+        else:
+            Boolean_Calculator.Execute(TestArray)
+    elif action == "E":
+        print("\n~Sair do Código~\n")
+        break
+    else:
+        print("Operação Inválida")
+        pass
